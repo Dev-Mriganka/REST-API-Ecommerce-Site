990e06ac32fe99662774e86a39f122cfc628f565
sushank678 <mandal678@gmail.com>
2022-11-19 16:32:53 +0530

    git fetch

--- Mini_Project/src/main/java/com/mini_project/model/Cart.java
+++ Mini_Project/src/main/java/com/mini_project/model/Cart.java
@@ -1,5 +1,6 @@
 package com.mini_project.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -11,6 +12,7 @@ import java.util.List;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class Cart {
 
     @Id
--- Mini_Project/src/main/java/com/mini_project/model/UserModel.java
+++ Mini_Project/src/main/java/com/mini_project/model/UserModel.java
@@ -1,5 +1,6 @@
 package com.mini_project.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -14,6 +15,7 @@ import java.util.Set;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class UserModel {
 
     @Id
